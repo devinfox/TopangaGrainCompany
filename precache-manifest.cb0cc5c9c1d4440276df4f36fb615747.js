3cc4d3e611d4bc261d2ce2c847b49686ff4f1489
Devin Fox <devinfoxbusiness@gmail.com>
2018-11-12 15:20:13 -0800

    Updates

---
+++ precache-manifest.cb0cc5c9c1d4440276df4f36fb615747.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ec4a3528bb6b0bd79edf9a1dc601fc7",
+    "url": "/TopangaGrainCompany/static/media/PlayfairDisplay-Regular.2ec4a352.ttf"
+  },
+  {
+    "revision": "eb3aa3d3dfaaee7c3b9d342b0c1da9e0",
+    "url": "/TopangaGrainCompany/static/media/Avenir.eb3aa3d3.ttc"
+  },
+  {
+    "revision": "704a616bbeb837c194d0",
+    "url": "/TopangaGrainCompany/static/js/runtime~main.704a616b.js"
+  },
+  {
+    "revision": "76ac9ce0a9dbbfceefc7",
+    "url": "/TopangaGrainCompany/static/js/main.76ac9ce0.chunk.js"
+  },
+  {
+    "revision": "256c7ab196bedca33bdb",
+    "url": "/TopangaGrainCompany/static/js/1.256c7ab1.chunk.js"
+  },
+  {
+    "revision": "76ac9ce0a9dbbfceefc7",
+    "url": "/TopangaGrainCompany/static/css/main.8dc0ae53.chunk.css"
+  },
+  {
+    "revision": "3bb752b142d470ec3bb53c54fcffed42",
+    "url": "/TopangaGrainCompany/index.html"
+  }
+];
