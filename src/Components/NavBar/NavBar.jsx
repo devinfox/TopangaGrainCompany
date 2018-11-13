695e015ed41a37852c42f90afbaa23e44c487985
Devin Fox <devinfoxbusiness@gmail.com>
2018-11-12 17:19:39 -0800

    header-fixed

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,12 +5,13 @@ import {Link} from 'react-router-dom';
 const NavBar = props => (
     <header className="NavBar">
         <nav className="NavBar_navigation">
-            <div></div>
-
+            
+            <div className="logo-mobile">
             <Link to='/'>
             <div className="NavBar_Logo"><img src="https://i.imgur.com/l6BBZBA.png" height="125" width="125" /></div>
             </Link>
-
+            </div>
+            
             <div className="spacer"></div>
             <div className="NavBar_items">
                 <ul>
